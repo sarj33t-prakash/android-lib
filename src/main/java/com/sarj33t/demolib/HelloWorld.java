663bfdd392f4b5d332e13569eafb268c887c3c27
Sarjeet Sandhu <psspl.trainee57@outlook.com>
2023-06-14 12:56:42 +0530

    upload

---
+++ src/main/java/com/sarj33t/demolib/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sarj33t.demolib;
+
+import android.util.Log;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World! This is Android Library test");
+    }
+
+    /// Prints the given message
+    public static void printMsg(String tag, String message){
+        //System.out.println(message);
+        Log.d(tag, message);
+    }
+}
