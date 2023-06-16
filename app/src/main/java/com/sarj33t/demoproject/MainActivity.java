307228dfc6d132c8f072a070a5fbe86c3009a052
Sarjeet Sandhu <psspl.trainee57@outlook.com>
2023-06-16 13:49:20 +0530

    added activity html

--- app/src/main/java/com/sarj33t/demoproject/MainActivity.java
+++ app/src/main/java/com/sarj33t/demoproject/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.sarj33t.firstlib.HelloWorld;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        HelloWorld.printMsg("", "");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
